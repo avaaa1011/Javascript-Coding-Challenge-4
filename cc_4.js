d5c7a99a0a2f8e1e8e89b5d81f66831d1e30743c
Ava Tran <ava31@usf.edu>
2025-02-02 13:51:36 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+//Task 1 : Customer Discounts 
+let purchaseAmount = 101;
+let discount = purchaseAmount;
+if (purchaseAmount > 100)
+    {
+    let discount = 10;
+    finalAmount = purchaseAmount - discount;
+    }
+console.log(`Final amount after discount: $${finalAmount}`);
+
