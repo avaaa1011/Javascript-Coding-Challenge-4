36c493faf89fd494d373ccf1eb77cd2a01b617a4
Ava Tran <ava31@usf.edu>
2025-02-02 14:11:30 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,16 @@ if (purchaseAmount > 100)
     }
 console.log(`Final amount after discount: $${finalAmount}`);
 
+//Task 2 : Sales Report
+let sales = [120, 85, 200, 150, 90]; //declaring arrray sales with five figures
+let totalSales = 0; 
+for(let i = 0; i < sales.length; i++) //using a for loop to calculate the total sales
+    {
+        totalSales += sales[i]; //this essentially adds a sales figure to the total everytime it loops
+    }
+    console.log(`Total Sales: $${totalSales}`); 
+
+
+
+
+
